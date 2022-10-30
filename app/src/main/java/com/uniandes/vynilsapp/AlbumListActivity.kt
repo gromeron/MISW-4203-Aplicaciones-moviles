5436712507401b7bbce6f7208695f37013e56987
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-10-29 19:52:14 -0500

    album list and detail added initially

---
+++ app/src/main/java/com/uniandes/vynilsapp/AlbumListActivity.kt
@@ -0,0 +1,11 @@
+package com.uniandes.vynilsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlbumListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_list)
+    }
+}
