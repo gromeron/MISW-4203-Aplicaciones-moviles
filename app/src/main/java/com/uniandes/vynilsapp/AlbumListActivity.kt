840ac3c024d21fcf2576a02ffafd31806720b036
Edinson Morales <e.moralesm2@uniandes.edu.co>
2022-10-30 19:29:42 -0500

    MERGE2DEVELOP

--- app/src/main/java/com/uniandes/vynilsapp/AlbumListActivity.kt
+++ app/src/main/java/com/uniandes/vynilsapp/AlbumListActivity.kt
@@ -3,45 +3,46 @@ package com.uniandes.vynilsapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import com.android.volley.Response
 import VolleyBroker
-import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import org.json.JSONArray
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.uniandes.vynilsapp.model.Album
 
-class AlbumListActivity : AppCompatActivity() {
+class AlbumListActivity : AppCompatActivity(), ListAdapter.OnItemClickListener {
 
     lateinit var volleyBroker: VolleyBroker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportActionBar?.hide()
+
         setContentView(R.layout.activity_album_list)
+
+        val reciclerView = findViewById<RecyclerView>(R.id.albumsView)
+        var albums = mutableListOf<Album>()
 
         volleyBroker = VolleyBroker(this.applicationContext)
 
         volleyBroker.instance.add(VolleyBroker.getRequest("albums",
             { response ->
                 val albumJsonResponse = JSONArray(response)
-                val albums = arrayOfNulls<String>(albumJsonResponse.length())
-                Log.d("ALBUMS", albumJsonResponse.toString())
+
                 for (i in 0 until albumJsonResponse.length()) {
                     val album = albumJsonResponse.getJSONObject(i)
-                    albums[i] = album.get("name").toString() + " | " +album.get("releaseDate").toString();
+                    albums.add(Album(album.get("name").toString(), album.get("releaseDate").toString(), R.drawable.ic_launcher_foreground))
                 }
-                // access the listView from xml file
-                val arrayAdapter: ArrayAdapter<*>
-                var mListView = findViewById<ListView>(R.id.albumlist)
-                arrayAdapter = ArrayAdapter(this,
-                    android.R.layout.simple_list_item_1, albums)
-                mListView.adapter = arrayAdapter
 
-            },
-            {
-                Log.d("errorMessage", it.toString())
-            }
+                val adapter = ListAdapter(this, albums)
+
+                reciclerView.layoutManager = LinearLayoutManager(this)
+                reciclerView.adapter = adapter
+            },{}
         ))
+    }
 
+    override fun onItemClick(position: Int) {
+        val intent = Intent(this@AlbumListActivity, AlbumDetailActivity::class.java)
+        startActivity(intent)
     }
 }
