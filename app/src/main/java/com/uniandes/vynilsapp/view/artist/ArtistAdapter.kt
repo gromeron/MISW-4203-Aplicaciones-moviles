166f11d52f7a704283de981b4b9b551c1218c3e0
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-11-11 01:53:39 -0500

    artistAdapter completed 3/3 artistViewHolder completed

--- app/src/main/java/com/uniandes/vynilsapp/view/artist/ArtistAdapter.kt
+++ app/src/main/java/com/uniandes/vynilsapp/view/artist/ArtistAdapter.kt
@@ -1,16 +1,20 @@
 package com.uniandes.vynilsapp.view.artist
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.uniandes.vynilsapp.R
 import com.uniandes.vynilsapp.data.model.Performer
 
-class ArtistAdapter(val artistList: List<Performer>): RecyclerView.Adapter<ArtistViewHolder>() {
+class ArtistAdapter(private val artistList: List<Performer>): RecyclerView.Adapter<ArtistViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtistViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ArtistViewHolder(layoutInflater.inflate(R.layout.item_artist, parent, false))
     }
 
     override fun onBindViewHolder(holder: ArtistViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = artistList[position]
+        holder.render(item)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/uniandes/vynilsapp/view/artist/ArtistViewHolder.kt
+++ app/src/main/java/com/uniandes/vynilsapp/view/artist/ArtistViewHolder.kt
@@ -1,4 +1,23 @@
 package com.uniandes.vynilsapp.view.artist
 
-class ArtistViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.uniandes.vynilsapp.R
+import com.uniandes.vynilsapp.data.model.Performer
+
+class ArtistViewHolder(view: View): ViewHolder(view) {
+
+    val artistName = view.findViewById<TextView>(R.id.tv_artist_name)
+    val artistIcon = view.findViewById<ImageView>(R.id.iv_artist_icon)
+
+    fun render(artistModel: Performer) {
+
+        artistName.text = artistModel.name
+        Glide.with(artistIcon.context).load(artistModel.image).into(artistIcon)
+
+    }
+
 }
