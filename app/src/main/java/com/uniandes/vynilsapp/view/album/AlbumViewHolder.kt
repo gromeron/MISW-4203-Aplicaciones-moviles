ed440d2699d908ef7da41c8cd8450c6c31465d9f
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-11-10 23:08:26 -0500

    data binding on AlbumViewModel

--- app/src/main/java/com/uniandes/vynilsapp/view/album/AlbumViewHolder.kt
+++ app/src/main/java/com/uniandes/vynilsapp/view/album/AlbumViewHolder.kt
@@ -7,18 +7,24 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.uniandes.vynilsapp.R
 import com.uniandes.vynilsapp.data.model.Album
+import com.uniandes.vynilsapp.databinding.ItemAlbumBinding
 
 class AlbumViewHolder(view: View): ViewHolder(view) {
 
-    val albumName = view.findViewById<TextView>(R.id.tv_album_name)
-    val albumGenre = view.findViewById<TextView>(R.id.tv_album_genre)
-    val albumIcon = view.findViewById<ImageView>(R.id.iv_album_icon)
+    val binding = ItemAlbumBinding.bind(view)
+
+    //val albumName = view.findViewById<TextView>(R.id.tv_album_name)
+    //val albumGenre = view.findViewById<TextView>(R.id.tv_album_genre)
+    //val albumIcon = view.findViewById<ImageView>(R.id.iv_album_icon)
 
     fun render(albumModel: Album) {
 
-        albumName.text = albumModel.name
-        albumGenre.text = albumModel.genre
-        Glide.with(albumIcon.context).load(albumModel.cover).into(albumIcon)
+        //albumName.text = albumModel.name
+        //albumGenre.text = albumModel.genre
+        //Glide.with(albumIcon.context).load(albumModel.cover).into(albumIcon)
+        binding.tvAlbumName.text = albumModel.name
+        binding.tvAlbumGenre.text = albumModel.genre
+        Glide.with(binding.ivAlbumIcon.context).load(albumModel.cover).into(binding.ivAlbumIcon)
 
     }
 }
