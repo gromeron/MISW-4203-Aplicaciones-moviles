e871aa0a20757f77c974b85516a1d424d9a756dc
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-11-11 00:42:10 -0500

    artist-list activity created

---
+++ app/src/main/java/com/uniandes/vynilsapp/view/artist/ArtistListActivity.kt
@@ -0,0 +1,12 @@
+package com.uniandes.vynilsapp.view.artist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uniandes.vynilsapp.R
+
+class ArtistListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist_list)
+    }
+}
