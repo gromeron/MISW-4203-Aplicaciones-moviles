677dba2bdd4ba8bf9571801ef4045060ff5f0352
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-11-10 23:48:03 -0500

    AlbumAdapter modified

--- app/src/main/java/com/uniandes/vynilsapp/view/album/AlbumAdapter.kt
+++ app/src/main/java/com/uniandes/vynilsapp/view/album/AlbumAdapter.kt
@@ -6,13 +6,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.uniandes.vynilsapp.R
 import com.uniandes.vynilsapp.data.model.Album
 
-class AlbumAdapter(private val albumList: List<Album>): RecyclerView.Adapter<AlbumViewHolder>() {
+class AlbumAdapter(/*private val albumList: List<Album>*/): RecyclerView.Adapter<AlbumViewHolder>() {
 
-    /*var albumList: List<Album> = listOf()
+    var albumList: List<Album> = listOf()
         set(value) {
             field = value
             notifyDataSetChanged()
-        }*/
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
