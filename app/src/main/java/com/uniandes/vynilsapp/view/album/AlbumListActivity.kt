f614e7dfbdbcfdc8f2a71388228ae86573ffcc17
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-11-10 23:03:02 -0500

    data binding on albumactivity

--- app/src/main/java/com/uniandes/vynilsapp/view/album/AlbumListActivity.kt
+++ app/src/main/java/com/uniandes/vynilsapp/view/album/AlbumListActivity.kt
@@ -6,14 +6,18 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.uniandes.vynilsapp.R
 import com.uniandes.vynilsapp.data.db.AlbumMock
+import com.uniandes.vynilsapp.databinding.ActivityAlbumListBinding
 
 class AlbumListActivity : AppCompatActivity() {
 
     private lateinit var albumAdapter: AlbumAdapter
+    private lateinit var binding: ActivityAlbumListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_album_list)
+        //setContentView(R.layout.activity_album_list)
+        binding = ActivityAlbumListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //albumAdapter = AlbumAdapter()
 
@@ -23,7 +27,9 @@ class AlbumListActivity : AppCompatActivity() {
     private fun initRecyclerView() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_album)
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = AlbumAdapter(AlbumMock.albumList)
+        //recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerView.adapter = AlbumAdapter(AlbumMock.albumList)
+        binding.recyclerAlbum.layoutManager = LinearLayoutManager(this)
+        binding.recyclerAlbum.adapter = AlbumAdapter(AlbumMock.albumList)
     }
 }
