9ff1d9a57fcc9e274e6c79016c98c1f23123cd98
Gustavo Romero <g.romeron2@uniandes.edu.co>
2022-11-10 21:35:18 -0500

    albumviewholder completed 2/3 without icon part

--- app/src/main/java/com/uniandes/vynilsapp/view/album/AlbumViewHolder.kt
+++ app/src/main/java/com/uniandes/vynilsapp/view/album/AlbumViewHolder.kt
@@ -1,11 +1,22 @@
 package com.uniandes.vynilsapp.view.album
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.uniandes.vynilsapp.R
 import com.uniandes.vynilsapp.data.model.Album
 
 class AlbumViewHolder(view: View) {
 
+    val albumName = view.findViewById<TextView>(R.id.tv_album_name)
+    val albumGenre = view.findViewById<TextView>(R.id.tv_album_genre)
+    val albumIcon = view.findViewById<ImageView>(R.id.iv_album_icon)
+
     fun render(albumModel: Album) {
-        
+
+        albumName.text = albumModel.name
+        albumGenre.text = albumModel.genre
+        // Por ahora no hacemos el icon
+
     }
 }
